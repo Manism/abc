1091664f818ade99489410bc1f02b4b1295f4e2c
Manism <sm.manicutee@outlook.com>
2016-10-20 13:08:18 +0530

    Update WebForm1.aspx.cs

--- abc/abc/WebForm1.aspx.cs
+++ abc/abc/WebForm1.aspx.cs
@@ -16,8 +16,10 @@ namespace abc
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
-
+               int a = Convert.ToInt32(TextBox1.Text);
+            int b = Convert.ToInt32(TextBox2.Text);
+            int c = a + b;
+            TextBox3.Text = Convert.ToString(c);
         }
     }
 }
